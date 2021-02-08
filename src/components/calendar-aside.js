f928b11c801d83cc7ecd740297c12bb56f4fad46
Brenton Kelly <brentonkelly1982@gmail.com>
2021-02-08 11:23:01 -0500

    Defined calendar aside component.

---
+++ src/components/calendar-aside.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendarAside = () => (
+    <aside className="google-calendar__aside">
+        <h3>Calendars</h3>
+        <ul className="google-calendar__calendar-list">
+            <li className="google-calendar__calendar-list-item">calendar 1</li>
+        </ul>
+    </aside>
+);
+
+export default CalendarAside;
