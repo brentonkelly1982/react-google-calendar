c455b1109bdc5511be0f93dbae13210af52124cb
Brenton Kelly <brentonkelly1982@gmail.com>
2021-02-08 13:57:49 -0500

    Added more date cell styling.

--- src/components/calendar-day.js
+++ src/components/calendar-day.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const CalendarDay = ({ blankDayCell, date, events }) => (
     <div className={(blankDayCell == "true") ? 'google-calendar__day google-calendar--empty-day' : 'google-calendar__day'}>
-        {(blankDayCell == "true") ? '' : date}
+        {(blankDayCell == "true") ? '' : <span className="google-calendar__cell-date">{date}</span>}
         {(blankDayCell != "true" && events.length) ?
-            <ul>
+            <ul className="google-calendar__events">
                 {events.map((event, index) => {
-                    return <li key={index}>{event.summary}</li>
+                    return <li className="google-calendar__event" key={index}>{event.summary}</li>
                 })}
             </ul>
             : ''
