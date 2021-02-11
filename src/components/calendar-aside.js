bb83ecc304c4425cf992e3c66186ba8ecd63968e
Brenton Kelly <brentonkelly1982@gmail.com>
2021-02-11 15:19:52 -0500

    Added calendar toggles in sidebar and added a few more styles to the list items.

--- src/components/calendar-aside.js
+++ src/components/calendar-aside.js
@@ -7,10 +7,12 @@ const CalendarAside = () => {
 
     return (
         <aside className="google-calendar__aside">
-            <h3>Calendars</h3>
+            <h3 className="google-calendar__calendar-list-header">Calendars</h3>
             <ul className="google-calendar__calendar-list">
                 {calendars.map((calendar, index) => {
                     return  <li className="google-calendar__calendar-list-item" key={index}>
+                                <label className="google-calendar__calendar-toggle-label google-calendar--ally-hidden" for={"google-calendar__calendar-item-" + index}>Toggle {calendar.name} calendar</label>
+                                <input type="checkbox" className="google-calendar__calendar-toggle" id={"google-calendar__calendar-item-" + index} />
                                 <span className="google-calendar__calendar-color-swatch" style={{backgroundColor: calendar.color}}></span>
                                 <span className="google-calendar__calendar-name">{calendar.name}</span>
                             </li>
