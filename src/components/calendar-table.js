2a6115178597b606193beee573fa58ccdae68d93
Brenton Kelly <brentonkelly1982@gmail.com>
2021-02-23 14:18:50 -0500

    Added aria-live attribute to calendar table.

--- src/components/calendar-table.js
+++ src/components/calendar-table.js
@@ -3,7 +3,7 @@ import CalendarHeaders from './calendar-headers';
 import CalendarRows from './calendar-rows';
 
 const CalendarTable = () => (
-    <div className="google-calendar__table">
+    <div className="google-calendar__table" aria-live="polite">
         <CalendarHeaders />
         <CalendarRows />
     </div>
